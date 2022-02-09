cf45c84b169b98d3252ee3f73e92a8d94af68aa7
will <wye139629@gmail.com>
2022-02-10 00:03:40 +0800

    feat: change twitter card img

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/blog.png',
   email: 'wye139629@gmail.com',
   github: 'https://github.com/wye139629',
   locale: 'en-US',
